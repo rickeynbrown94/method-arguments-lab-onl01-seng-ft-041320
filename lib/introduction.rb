420d99247462a36c2d148838e1c6ccabf39cfecc
Rickey Brown <rickeynbrown94@gmail.com>
2020-04-20 23:47:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(name, name)
-  puts "HI, my name is #{name} and I am learning to program in #{language}."
+def introduction_with_language(name1, language)
+  puts "HI, my name is #{name1} and I am learning to program in #{language}."
 end
